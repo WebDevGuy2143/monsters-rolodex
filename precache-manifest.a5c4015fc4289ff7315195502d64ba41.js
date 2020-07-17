dcd2173aec874579dd080252be6d7a46c91b9e0b
Gene Howell <gene.howell9@gmail.com>
2020-07-17 11:17:49 -0500

    Updates

---
+++ precache-manifest.a5c4015fc4289ff7315195502d64ba41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac8f55f87164a2b05d71617b04c861b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd6d571eb4d3c0498048",
+    "url": "/monsters-rolodex/static/css/main.eff4be7a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6d571eb4d3c0498048",
+    "url": "/monsters-rolodex/static/js/main.5f71bbf6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
